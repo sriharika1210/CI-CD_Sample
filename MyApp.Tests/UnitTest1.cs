8741c0593d939c357fbff49410dacaf2ead23d16
nharika546 <sriharikanuka@gmail.com>
2025-11-18 15:22:50 +0530

    Initial commit

---
+++ MyApp.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿namespace MyApp.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Add_ShouldReturnCorrectSum()
+    {
+        var calc = new Calculator();
+        Assert.Equal(8, calc.Add(5, 3));
+    }
+
+    [Fact]
+    public void Subtract_ShouldReturnCorrectDifference()
+    {
+        var calc = new Calculator();
+        Assert.Equal(2, calc.Subtract(5, 3));
+    }
+}
---
+++ MyApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Calculator();
+            Console.WriteLine($"5 + 3 = {calc.Add(5, 3)}");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine(calc.Multiply(2,age));
+        }
+    }
+}
