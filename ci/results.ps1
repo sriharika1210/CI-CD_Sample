d188311e5a8551bebb477a220cf1f4b83342c065
N.Harika <sriharika1210@gmail.com>
2025-11-25 21:34:59 +0530

    change manual

--- ci/results.ps1
+++ ci/results.ps1
@@ -2,8 +2,6 @@ Write-Host "=== Generating Coverage Report ==="
 
 dotnet tool install -g dotnet-reportgenerator-globaltool
 
-reportgenerator -reports:**/coverage.cobertura.xml -targetdir:CoverageReport -reporttypes:Html
-
-reportgenerator -reports:**/coverage.cobertura.xml -targetdir:CoverageReport -reporttypes:Html
+reportgenerator -reports:manual_coverage.cobertura.xml -targetdir:CoverageReport -reporttypes:Html
 
 Write-Host "Coverage Report generated!"
