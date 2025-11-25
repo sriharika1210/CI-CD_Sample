6ec6f35cfbde166fcb64d1bf5d5cfaf1ef7b3cb0
N.Harika <sriharika1210@gmail.com>
2025-11-25 21:02:17 +0530

    Manual Add

--- ci/test.ps1
+++ ci/test.ps1
@@ -4,10 +4,7 @@ Write-Host "Running Tests..."
 dotnet test --configuration Release --collect:"XPlat Code Coverage" --results-directory "./TestResults"
 
 # Manual Tests With Code Coverage
-dotnet-coverage collect `
-    --output-format cobertura `
-    --output ../coverage.cobertura.xml `
-    -- dotnet ../MyApp/bin/Debug/net9.0/MyApp.dll `
-    < ../inputs/ageInput.txt
+Get-Content "../inputs/ageInput.txt" | dotnet-coverage collect --output-format cobertura --output "../coverage.cobertura.xml" -- dotnet "../MyApp/bin/Debug/net9.0/MyApp.dll"
+
 
 Write-Host "Tests completed!"
