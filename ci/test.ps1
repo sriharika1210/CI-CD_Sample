4c8d86af769dd037f451f54dc619f66210b88710
N.Harika <sriharika1210@gmail.com>
2025-11-25 21:38:50 +0530

    change2

--- ci/test.ps1
+++ ci/test.ps1
@@ -4,7 +4,7 @@ Write-Host "Running Tests..."
 dotnet test --configuration Release --collect:"XPlat Code Coverage" --results-directory "./TestResults"
 
 # Manual Tests With Code Coverage
-Get-Content "../inputs/ageInput.txt" | dotnet-coverage collect --output-format cobertura --output "../coverage.cobertura.xml" -- dotnet "../MyApp/bin/Debug/net9.0/MyApp.dll"
+Get-Content "../inputs/ageInput.txt" | dotnet-coverage collect --output-format cobertura --output "../manual_coverage.cobertura.xml" -- dotnet "../MyApp/bin/Debug/net9.0/MyApp.dll"
 
 
 Write-Host "Tests completed!"
