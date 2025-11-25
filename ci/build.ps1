962599dcb4e3353d21de498ea9246d93ef12c3cb
N.Harika <sriharika1210@gmail.com>
2025-11-25 15:31:28 +0530

    add run

--- ci/build.ps1
+++ ci/build.ps1
@@ -3,6 +3,9 @@ Write-Host "Starting Build..."
 # Restore packages
 dotnet restore
 
+#Run solution
+- run: echo 25 | dotnet run --project MyApp
+
 # Build solution
 dotnet build --configuration Release
 
