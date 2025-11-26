16ced618a4bac744e8b827e7a7aff59fe3197d2e
N.Harika <sriharika1210@gmail.com>
2025-11-26 12:12:21 +0530

    changeddddddddddddd

--- ci/results.ps1
+++ ci/results.ps1
@@ -2,6 +2,6 @@ Write-Host "=== Generating Coverage Report ==="
 
 dotnet tool install -g dotnet-reportgenerator-globaltool
 
-reportgenerator -reports:"*.xml" -targetdir:"CoverageReport" -reporttypes:"Html"
+reportgenerator -reports:"**/*.xml" -targetdir:"CoverageReport" -reporttypes:"Html"
 
 Write-Host "Coverage Report generated!"
