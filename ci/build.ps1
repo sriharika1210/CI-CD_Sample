71934a54be0310f11a1e10c209a8a853d822873b
N.Harika <sriharika1210@gmail.com>
2025-11-26 15:32:16 +0530

    chagedsdaf

--- ci/build.ps1
+++ ci/build.ps1
@@ -6,9 +6,6 @@ dotnet restore
 #Install Package
 dotnet tool install -g dotnet-coverage
 
-#Run solution
-Get-Content "./inputs/ageInput.txt" | dotnet run --project MyApp
-
 # Build solution
 dotnet build --configuration Release
 
