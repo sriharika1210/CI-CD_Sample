480da705735a599bb7bcae8a8ec7b1a8e823cd17
N.Harika <sriharika1210@gmail.com>
2025-11-26 10:43:08 +0530

    mmm

--- ci/test.ps1
+++ ci/test.ps1
@@ -2,7 +2,7 @@ Write-Host "Running Tests..."
 
 
 # Manual Tests With Code Coverage
-Get-Content "./inputs/ageInput.txt" | dotnet-coverage collect --output-format cobertura --output "../manual_coverage.cobertura.xml" -- dotnet "../MyApp/bin/Release/net9.0/win-x64/MyApp.dll"
+Get-Content "./inputs/ageInput.txt" | dotnet-coverage collect --output-format cobertura --output "../manual_coverage.cobertura.xml" -- dotnet "../MyApp/bin/Release/net9.0/MyApp.dll"
 
 
 Write-Host "Tests completed!"
