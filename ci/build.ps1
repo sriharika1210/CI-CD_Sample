521d77d988f5ba6d6562e14e2afeb32b3dbd37e4
N.Harika <sriharika1210@gmail.com>
2025-11-26 10:21:52 +0530

    add

--- ci/build.ps1
+++ ci/build.ps1
@@ -3,6 +3,9 @@ Write-Host "Starting Build..."
 # Restore packages
 dotnet restore
 
+#Install Package
+dotnet tool install -g dotnet-coverage
+
 #Run solution
 Get-Content "./inputs/ageInput.txt" | dotnet run --project MyApp
 
