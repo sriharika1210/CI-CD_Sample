fd935198e3947a5a00f57d1caea3c9ab0de24647
N.Harika <sriharika1210@gmail.com>
2025-11-26 10:07:04 +0530

    manual check

--- ci/test.ps1
+++ ci/test.ps1
@@ -1,7 +1,5 @@
 Write-Host "Running Tests..."
 
-# Run Unit Tests
-dotnet test --configuration Release --collect:"XPlat Code Coverage" --results-directory "./TestResults"
 
 # Manual Tests With Code Coverage
 Get-Content "../inputs/ageInput.txt" | dotnet-coverage collect --output-format cobertura --output "../manual_coverage.cobertura.xml" -- dotnet "../MyApp/bin/Debug/net9.0/MyApp.dll"
