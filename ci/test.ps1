4569f25e815dfb154ebc7490fa5f6726672f5470
N.Harika <sriharika1210@gmail.com>
2025-11-28 10:56:15 +0530

    docker

--- ci/test.ps1
+++ ci/test.ps1
@@ -1,19 +1,20 @@
+param(
+    [string]$workspace
+)
+
 Write-Host "Running Tests..."
 
 # Build test Docker image
 docker build -f Dockerfile.tests -t myapp-tests .
 
-# Run Unit Tests
-#dotnet test --configuration Release --collect:"XPlat Code Coverage" --results-directory "./TestResults"
-mkdir TestResults
-        mkdir CoverageReport
-        docker run --rm \
-          -v ${{ github.workspace }}/TestResults:/src/TestResults \
-          -v ${{ github.workspace }}/CoverageReport:/src/CoverageReport \
-          myapp-tests
+# Prepare folders
+mkdir "$workspace/TestResults" -Force
+mkdir "$workspace/CoverageReport" -Force
 
-# Manual Tests With Code Coverage
-Get-Content "./inputs/ageInput.txt" | dotnet-coverage collect --output-format cobertura --output "../manual_coverage.cobertura.xml" -- dotnet "./MyApp/bin/Release/net9.0/MyApp.dll"
-
+# Run Unit Tests inside Docker
+docker run --rm `
+    -v "$workspace/TestResults:/src/TestResults" `
+    -v "$workspace/CoverageReport:/src/CoverageReport" `
+    myapp-tests
 
 Write-Host "Tests completed!"
